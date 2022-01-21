614748969486ebc4591e2ae86ace61a226643a47
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-21 16:21:08 +0200

    Были сделаны некоторые таски

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
 const DEBOUNCE_DELAY = 300;
+
+const findCountriesEl = document.querySelector('#search-box');
+const countriesListEl = document.querySelector('.country-list');
+const countryInfoEl = document.querySelector('.country-info');
+
+const onInputInput = event => {
+    const country = event.target.value.trim();
+    fetchCountries(country)
+    .then(data => console.log(data))
+    .catch(err => console.log(err));
+};
+
+findCountriesEl.addEventListener('input', debounce(onInputInput, DEBOUNCE_DELAY));
+
+
+
