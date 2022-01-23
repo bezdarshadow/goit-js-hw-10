afa894a087cb47bfcee1b6b29369b47e21cfe1c1
Danyil Dykyi <91718159+bezdarshadow@users.noreply.github.com>
2022-01-23 13:48:39 +0200

    Был сделан поиск стран по запросу на api RestCountries

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import './css/styles.css';
 import { fetchCountries } from './js/fetchCountries';
 import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
+import allCountries from './templates/all-countries.hbs';
+import currentCountry from './templates/current-country.hbs';
 
 const DEBOUNCE_DELAY = 300;
 
@@ -8,11 +11,32 @@ const findCountriesEl = document.querySelector('#search-box');
 const countriesListEl = document.querySelector('.country-list');
 const countryInfoEl = document.querySelector('.country-info');
 
+
 const onInputInput = event => {
-    const country = event.target.value.trim();
+    const country = findCountriesEl.value.trim();
+    if(country === ''){
+        countriesListEl.innerHTML = "";
+        countryInfoEl.innerHTML = "";
+        return;
+    }
     fetchCountries(country)
-    .then(data => console.log(data))
-    .catch(err => console.log(err));
+    .then(data => {
+        console.log(data)
+        if(data.length <= 10 && data.length >= 2){
+            countryInfoEl.innerHTML = "";
+            countriesListEl.innerHTML = allCountries(data);    
+        } else if (data.length === 1) {
+            countriesListEl.innerHTML = "";
+            countryInfoEl.innerHTML = currentCountry(data);
+        } else if (data.length > 10) {
+            Notiflix.Notify.info("Too many matches found. Please enter a more specific name.")
+        }
+    })
+    .catch(err => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+        countryInfoEl.innerHTML = "";
+        countriesListEl.innerHTML = "";
+    })
 };
 
 findCountriesEl.addEventListener('input', debounce(onInputInput, DEBOUNCE_DELAY));
--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -1,13 +1,15 @@
 'use strict';
 
-const BASE_URL = 'https://restcountries.com/v2/name'; 
+const BASE_URL = 'https://restcountries.com/v3.1/name'; 
 
 export const fetchCountries = name => {
-    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,languages,flags`).then(response => {
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,languages,flags`)
+    .then(response => {
         if (!response.ok) {
           throw new Error(response.status);
         }
     
         return response.json();
-      });
+    });
 };
+
